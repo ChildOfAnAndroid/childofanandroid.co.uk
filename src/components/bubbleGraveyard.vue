38df045c129ef413fc27998684b54e2b20e31391
Charis Cat <65819058+ChildOfAnAndroid@users.noreply.github.com>
2025-08-13 17:30:03 +0100

    file for bubble graveyard

---
+++ src/components/bubbleGraveyard.vue
@@ -0,0 +1,35 @@
+<!-- CHARIS CAT // CHILD OF AN ANDROID - 2025 -->
+<template>
+  <div class="bubble-graveyard-global">
+    <div
+      v-for="ghost in bbyState.graveyardBubbles"
+      :key="ghost.id"
+      class="ghost-bubble"
+      :style="{
+        top: ghost.startY,
+        left: ghost.startX,
+        width: ghost.width,
+        '--dest-x': ghost.ghostX,
+        '--dest-y': ghost.ghostY,
+        '--dest-r': ghost.ghostR,
+        '--ghost-duration': ghost.duration,
+        '--ghost-easing': ghost.easing,
+        '--ghost-delay': ghost.delay,
+        '--ghost-opacity1': ghost.ghostOpacity1,
+        '--ghost-opacity2': ghost.ghostOpacity2,
+        '--ghost-blur': ghost.ghostBlur,
+        backgroundColor: ghost.bgColour,
+        borderColor: ghost.borderColour,
+      }"
+    >
+      <span v-html="ghost.text"></span>
+      <strong class="bubble-author"> {{ ghost.author }}</strong>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { bbyUse } from '@/composables/bbyUse.ts';
+
+const { bbyState } = bbyUse();
+</script>
